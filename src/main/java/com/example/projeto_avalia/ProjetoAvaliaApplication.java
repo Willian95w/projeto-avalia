94361901eedd3a16635ca677957d5c3902bfb02e
WillianZD <willianzhang44@gmail.com>
2025-11-23 17:45:58 -0300

    chore: criar estrutura inicial do projeto avalia

---
+++ src/main/java/com/example/projeto_avalia/ProjetoAvaliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_avalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAvaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAvaliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto_avalia/ProjetoAvaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_avalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAvaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
