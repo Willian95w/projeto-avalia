05f12b02cb18f3cc7500d5571d265c835f99f1fc
WillianZD <willianzhang44@gmail.com>
2025-11-24 12:42:51 -0300

    feat: criar DTOs para registro, login e autenticação

---
+++ src/main/java/com/example/projeto_avalia/dto/AuthResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.projeto_avalia.dto;
+
+public record AuthResponseDTO(
+        String token
+) {}
---
+++ src/main/java/com/example/projeto_avalia/dto/RegisterDTO.java
@@ -0,0 +1,8 @@
+package com.example.projeto_avalia.dto;
+
+public record RegisterDTO(
+        String name,
+        String email,
+        String password,
+        String phone
+) {}
