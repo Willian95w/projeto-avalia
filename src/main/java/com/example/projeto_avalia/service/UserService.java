3d3e29ed45cc16840e67bc29359a91dc4b00325f
WillianZD <willianzhang44@gmail.com>
2025-11-24 19:30:17 -0300

    refactor: alterar mensagem de erro

--- src/main/java/com/example/projeto_avalia/service/UserService.java
+++ src/main/java/com/example/projeto_avalia/service/UserService.java
@@ -16,6 +16,6 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
     }
 }
