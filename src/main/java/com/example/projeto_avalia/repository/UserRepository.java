ac5c7366245a2c1a158cad848315e858af66951b
WillianZD <willianzhang44@gmail.com>
2025-11-24 12:22:03 -0300

    feat: criar UserRepository para persistência de dados de users

---
+++ src/main/java/com/example/projeto_avalia/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.projeto_avalia.repository;
+
+import com.example.projeto_avalia.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
