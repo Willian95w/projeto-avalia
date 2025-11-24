fa7d19d675e7f97bc651ce35e05da0aa3341c2e6
WillianZD <willianzhang44@gmail.com>
2025-11-24 16:51:26 -0300

    feat: criar DisciplinaService com operações de CRUD e busca

---
+++ src/main/java/com/example/projeto_avalia/service/DisciplinaService.java
@@ -0,0 +1,51 @@
+package com.example.projeto_avalia.service;
+
+import com.example.projeto_avalia.exceptions.BadRequestException;
+import com.example.projeto_avalia.exceptions.ResourceNotFoundException;
+import com.example.projeto_avalia.model.Disciplina;
+import com.example.projeto_avalia.repository.DisciplinaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DisciplinaService {
+
+    private final DisciplinaRepository disciplinaRepository;
+
+    public Disciplina criarDisciplina(String name) {
+        disciplinaRepository.findByName(name)
+                .ifPresent(d -> { throw new BadRequestException("Disciplina já existe"); });
+
+        Disciplina disciplina = Disciplina.builder()
+                .name(name)
+                .build();
+        return disciplinaRepository.save(disciplina);
+    }
+
+    public List<Disciplina> listarDisciplinas() {
+        return disciplinaRepository.findAllByOrderByIdDesc();
+    }
+
+    public Disciplina editarDisciplina(Long id, String newName) {
+        Disciplina disciplina = disciplinaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Disciplina não encontrada"));
+
+        disciplina.setName(newName);
+        return disciplinaRepository.save(disciplina);
+    }
+
+    public void excluirDisciplina(Long id) {
+        Disciplina disciplina = disciplinaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Disciplina não encontrada"));
+
+        disciplinaRepository.delete(disciplina);
+    }
+
+    public List<Disciplina> buscarPorNome(String name) {
+        return disciplinaRepository.findByNameContainingIgnoreCaseOrderByNameAsc(name);
+    }
+
+}
