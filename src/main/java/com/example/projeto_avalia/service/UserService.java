9b44241f26cf573830f536fe0e9aa8d98d881c34
WillianZD <willianzhang44@gmail.com>
2025-11-24 12:23:55 -0300

    feat: criar UserService para autenticação dos usuários

---
+++ src/main/java/com/example/projeto_avalia/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.projeto_avalia.service;
+
+import com.example.projeto_avalia.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
