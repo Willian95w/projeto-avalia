33bd8dbd176ae0ed018938a554a63439e921a832
WillianZD <willianzhang44@gmail.com>
2025-11-25 15:57:55 -0300

    feat: criar entidade Professor

---
+++ src/main/java/com/example/projeto_avalia/model/Professor.java
@@ -0,0 +1,38 @@
+package com.example.projeto_avalia.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "professores")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String phone;
+
+    @ManyToMany
+    @JoinTable(
+            name = "professor_disciplinas",
+            joinColumns = @JoinColumn(name = "professor_id"),
+            inverseJoinColumns = @JoinColumn(name = "disciplina_id")
+    )
+    private List<Disciplina> subjects;
+}
