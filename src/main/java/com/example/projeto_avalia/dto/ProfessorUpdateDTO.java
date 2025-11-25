b0044ffc8975c2da9074a9231e18810cef63c6db
WillianZD <willianzhang44@gmail.com>
2025-11-25 16:00:16 -0300

    feat: criar ProfessorUpdateDTO para atualização de professores

---
+++ src/main/java/com/example/projeto_avalia/dto/ProfessorUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.example.projeto_avalia.dto;
+
+import java.util.List;
+
+public record ProfessorUpdateDTO(
+        String name,
+        String phone,
+        String email,
+        List<Long> disciplinaIds
+) {}
