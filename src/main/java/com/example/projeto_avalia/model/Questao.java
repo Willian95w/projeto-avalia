7c00468a9a6c59dc8afdb0a34c035d47f2a9da50
WillianZD <willianzhang44@gmail.com>
2025-11-25 19:52:28 -0300

    feat: criar entidade Questao

---
+++ src/main/java/com/example/projeto_avalia/model/Questao.java
@@ -0,0 +1,59 @@
+package com.example.projeto_avalia.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Questao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String answerA;
+
+    @Column(nullable = false)
+    private String answerB;
+
+    @Column(nullable = false)
+    private String answerC;
+
+    @Column(nullable = false)
+    private String answerD;
+
+    @Column(nullable = false)
+    private String answerE;
+
+    @Column(nullable = false)
+    private String correctAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "discipline_id", nullable = false)
+    private Disciplina subject;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User createdBy;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
