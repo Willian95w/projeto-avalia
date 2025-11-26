bd7d4eb3936103fd58c4671a44475dc05e38b36c
WillianZD <willianzhang44@gmail.com>
2025-11-26 18:03:43 -0300

    refactor: remover métodos não utilizados de ProvaRepository

--- src/main/java/com/example/projeto_avalia/repository/ProvaRepository.java
+++ src/main/java/com/example/projeto_avalia/repository/ProvaRepository.java
@@ -4,11 +4,6 @@ import com.example.projeto_avalia.model.Prova;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProvaRepository extends JpaRepository<Prova, Long> {
-    List<Prova> findAllByOrderByIdDesc();
-
-    List<Prova> findByCreatedByIdOrderByIdDesc(Long creatorId);
 }
