8ccf38abce673f0d1e464341cb56f3bd1413e348
WillianZD <willianzhang44@gmail.com>
2025-11-26 12:00:40 -0300

    chore: permitir todas as origens no CORS

--- src/main/java/com/example/projeto_avalia/util/CorsConfiguration.java
+++ src/main/java/com/example/projeto_avalia/util/CorsConfiguration.java
@@ -10,6 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedOrigins("https://projeto-avalia-coordenador.vercel.app")
                 .allowedOrigins("https://projeto-avalia-professor.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
