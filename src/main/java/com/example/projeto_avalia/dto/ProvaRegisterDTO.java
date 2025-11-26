b29821b486d1ae063b27221bc718019898b2f811
Vinícius Alabarce <vinialabarce06@gmail.com>
2025-11-26 00:42:06 -0300

    feat: criar DTO para registro de prova

---
+++ src/main/java/com/example/projeto_avalia/dto/ProvaRegisterDTO.java
@@ -0,0 +1,15 @@
+package com.example.projeto_avalia.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ProvaRegisterDTO(
+        String titulo,
+        List<Long> idsQuestoes,
+        Long disciplinaId,
+        LocalDateTime dataProva,
+        Long professorResponsavelId,
+        String tipoAvaliacao,
+        Integer duracao,
+        Double peso
+) {}
